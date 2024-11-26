da4c842263b7b4455672795b5bd3d000b4058646
pablo <devpabloh@gmail.com>
2024-11-26 18:38:53 -0300

    criando os times e atribuindo um map para definir mais rápido os nomes e as cores de cada time

--- src/App.js
+++ src/App.js
@@ -4,6 +4,44 @@ import Formulario from "./componentes/Formulario";
 import Time from "./componentes/Time";
 
 function App() {
+  const times = [
+    {
+      nome:'Programação',
+      corPrimaria: '#57C278',
+      corSecundaria: '#D9F7E9'
+    },
+    {
+      nome:'Front-end',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF'
+    },
+    {
+      nome:'Data Science',
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2'
+    },
+    {
+      nome:'Devops',
+      corPrimaria: '#E06B69',
+      corSecundaria: '#FDE7E8'
+    },
+    {
+      nome:'Ux e design',
+      corPrimaria: '#DB6EBF',
+      corSecundaria: '#FAE9F5'
+    },
+    {
+      nome:'Mobile',
+      corPrimaria: '#FFBA05',
+      corSecundaria: '#FFF5D9'
+    },
+    {
+      nome:'Inovação e Gestão',
+      corPrimaria: '#FF8A29',
+      corSecundaria: '#FFEEDF'
+    },
+  ]
+
   const [colaboradores, setColaboradores] = useState([])
 
   const aoNovoColaboradorAdicionado = (colaborador)=>{
@@ -15,9 +53,8 @@ function App() {
     <div className="App">
         <Banner/>
         <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
-        <Time nome="Programação"/>
-        <Time nome="Front-end"/>
-        <Time nome="Data Science"/>
+        {times.map(time => <Time nome={time.nome}/>)}
+        
     </div>
   );
 }
