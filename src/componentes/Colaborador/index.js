b073a2e592c65150d39d8329c6e1d948e4a7a030
Pablo Henrique <pablohenriqueadm94@gmail.com>
2024-11-27 11:51:58 -0300

    Criando componente colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,16 @@
+import './Colaborador.css'
+const Colaborador = ()=>{
+    return(
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src="https://github.com/devpabloh.png" alt="Pablo Henrique" />
+            </div>
+            <div className='rodape'>
+                <h4>Pablo Henrique</h4>
+                <h5>Desenvolvedor</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,9 +1,12 @@
+import Colaborador from "../Colaborador"
 import "./Time.css"
 
 const Time = (props)=>{
+    const css = {backgroundColor:props.corSecundaria} //primeira forma de fazer criando uma variavel que vai receber um objeto javascript
     return(
-        <section className="time">
-            <h3>{props.nome}</h3>
+        <section className="time" style={css}> {/* Atribuindo essa variável que foi criada */}
+            <h3 style={{borderColor:props.corPrimaria}}>{props.nome}</h3> {/* Segunda forma de fazer, que é um estilo inline*/}
+            <Colaborador/>
         </section>
     )
 }
