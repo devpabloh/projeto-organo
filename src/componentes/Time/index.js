cd146c1943480918518aca9be5999aef3fdb03c0
Pablo Henrique <pablohenriqueadm94@gmail.com>
2024-11-28 18:08:37 -0300

    Update index.js

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -4,13 +4,13 @@ import "./Time.css"
 const Time = (props)=>{
     const css = {backgroundColor: props.corSecundaria} //primeira forma de fazer criando uma variavel que vai receber um objeto javascript
     return(
-        (props.colaboradores.length > 0) ? <section className="time" style={css}> {/* Atribuindo essa variável que foi criada */}
+        props.colaboradores.length > 0 && <section className="time" style={css}> {/* Atribuindo essa variável que foi criada */}
             <h3 style={{borderColor:props.corPrimaria}}>{props.nome}</h3> {/* Segunda forma de fazer, que é um estilo inline*/}
         <div className="colaboradores">
             {props.colaboradores.map(colaborador => <Colaborador corDeFundo={props.corPrimaria} key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/>)}
         </div>
         </section>
-        : ''
+        
     )
 }
 
